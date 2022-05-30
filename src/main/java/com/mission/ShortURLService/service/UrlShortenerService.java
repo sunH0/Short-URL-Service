e889db07242d65671dc866e54da11514fd270103
healtheefart <961210eee@naver.com>
2022-05-30 18:52:42 +0900

    Feat : created UrlShortenerService

---
+++ src/main/java/com/mission/ShortURLService/service/UrlShortenerService.java
@@ -0,0 +1,37 @@
+package com.mission.ShortURLService.service;
+
+import com.mission.ShortURLService.dto.ShortenUrlRequest;
+import com.mission.ShortURLService.dto.ShortenUrlResponse;
+import com.mission.ShortURLService.entity.ShortenUrl;
+import com.mission.ShortURLService.repository.UrlShortenerRepository;
+import com.mission.ShortURLService.utils.BASE62Utils;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UrlShortenerService {
+
+	private final BASE62Utils base62Utils;
+	private final UrlShortenerRepository urlShortenerRepository;
+
+	public ShortenUrlResponse getShortenUrl(ShortenUrlRequest request) {
+		Optional<ShortenUrl> optional = urlShortenerRepository.findByOriginUrl(request.getOriginUrl());
+		if (optional.isPresent()) {
+			return new ShortenUrlResponse(
+				request.getOriginUrl(),
+				base62Utils.encode(optional.orElseThrow(() -> new RuntimeException()).getId()));
+		}
+		ShortenUrl shortenUrl = urlShortenerRepository.save(new ShortenUrl(request.getOriginUrl()));
+		return new ShortenUrlResponse(request.getOriginUrl(), base62Utils.encode(shortenUrl.getId()));
+	}
+
+	public String getOriginUrl(String encodedUrl) {
+		Optional<ShortenUrl> optional = urlShortenerRepository.findById(base62Utils.decode(encodedUrl));
+		if (optional.isEmpty()) {
+			throw new RuntimeException();
+		}
+		return optional.orElseThrow(() -> new RuntimeException()).getOriginUrl();
+	}
+}

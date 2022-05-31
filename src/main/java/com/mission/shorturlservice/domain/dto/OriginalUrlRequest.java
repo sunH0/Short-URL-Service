7e20f554cf5b600b49ad932dffb48e58f426b93b
sunH0 <sunho6824@daum.net>
2022-05-31 17:47:59 +0900

    fix: request Dto 기본생성자 추가

--- src/main/java/com/mission/shorturlservice/domain/dto/OriginalUrlRequest.java
+++ src/main/java/com/mission/shorturlservice/domain/dto/OriginalUrlRequest.java
@@ -3,14 +3,16 @@ package com.mission.shorturlservice.domain.dto;
 import com.mission.shorturlservice.domain.entity.Url;
 import javax.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.URL;
 
 @Getter
+@NoArgsConstructor
 public class OriginalUrlRequest {
 
 	@URL
 	@NotBlank
-	private final String original;
+	private String original;
 
 	public OriginalUrlRequest(String original) {
 		this.original = original;

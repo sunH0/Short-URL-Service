035d28b69094cc6bceda1efb86cf863b8e325b15
sunH0 <sunho6824@daum.net>
2022-05-31 17:48:29 +0900

    refactor: api url 수정

--- src/main/java/com/mission/shorturlservice/domain/controller/UrlController.java
+++ src/main/java/com/mission/shorturlservice/domain/controller/UrlController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/shortUrl")
+@RequestMapping("/shortner")
 @RestController
 public class UrlController {
 

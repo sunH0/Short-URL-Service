d7d85f9bbd47c3ebce8cd8e3d6883d534e3ff437
healtheefart <961210eee@naver.com>
2022-06-02 06:54:24 +0900

    refactor : util관련 값을 설정파일로 관리

--- src/main/java/com/mission/ShortURLService/utils/BASE62Utils.java
+++ src/main/java/com/mission/ShortURLService/utils/BASE62Utils.java
@@ -1,32 +1,35 @@
 package com.mission.ShortURLService.utils;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BASE62Utils {
 
-	private static final String HEAD = "http://localhost:8080/";
-	private static final char[] BASE62 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
+	@Value("${shorten-url-suffix}")
+	private static String HEAD;
+	@Value("${shorten-algorithm-BASE62}")
+	private static String BASE62;
 
-	public static String encode(Integer value) {
+	public static String encode(String value) {
 		StringBuilder sb = new StringBuilder(HEAD);
+		Integer intVal = Integer.valueOf(value);
 		do {
-			Integer i = value % 62;
-			sb.append(BASE62[i]);
-			value /= 62;
-		} while (value > 0);
+			Integer i = intVal % 62;
+			sb.append(BASE62.charAt(i));
+			intVal /= 62;
+		} while (intVal > 0);
 		return sb.toString();
 	}
 	public static Integer decode(String value) {
 		Integer result = 0;
 		Integer power = 1;
 		for (int i = 0; i < value.length(); i++) {
-			int digit = new String(BASE62).indexOf(value.charAt(i));
+			int digit = BASE62.indexOf(value.charAt(i));
 			result += digit * power;
 			power *= 62;
 		}
 		return result;
 	}
-
 
 }

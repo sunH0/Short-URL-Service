92071f013cf9b36f7069b4c4f8a047e95f49cdb1
sunH0 <sunho6824@daum.net>
2022-06-01 17:58:49 +0900

    feat: Url index 설정

--- src/main/java/com/mission/shorturlservice/domain/entity/Url.java
+++ src/main/java/com/mission/shorturlservice/domain/entity/Url.java
@@ -1,10 +1,12 @@
 package com.mission.shorturlservice.domain.entity;
 
+import com.mission.shorturlservice.domain.common.BaseEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.Lob;
 import javax.persistence.Table;
 import lombok.AccessLevel;
@@ -17,8 +19,8 @@ import org.hibernate.annotations.DynamicInsert;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
-@Table(name = "url")
-public class Url {
+@Table(name = "url", indexes = @Index(name = "i_url", columnList = "count DESC, modified_at"))
+public class Url extends BaseEntity{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)

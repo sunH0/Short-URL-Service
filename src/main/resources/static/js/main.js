2624280462f19b8fdb3dab9f75d543ec645baa97
sunH0 <sunho6824@daum.net>
2022-06-01 18:42:18 +0900

    feat: url에 대한 요청 통계 frontEnd 코드 수정

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -9,6 +9,7 @@ ShortenURLUnit.prototype = {
     let urlText = document.getElementById('url-input');
     let urlButton = document.getElementById('url-button');
     let result =  document.getElementById('result').lastChild.previousSibling;
+    let count =  document.getElementById('count').lastChild.previousSibling;
 
     var httpRequest = new XMLHttpRequest();
     urlButton.addEventListener('click', function(){
@@ -16,6 +17,7 @@ ShortenURLUnit.prototype = {
         if (httpRequest.readyState == XMLHttpRequest.DONE && httpRequest.status == 200 ) {
           let urlShorten = JSON.parse(httpRequest.responseText);
           result.value = urlShorten.data.shortUrl;
+          count.value = urlShorten.data.count;
         }
       }
       let data = {

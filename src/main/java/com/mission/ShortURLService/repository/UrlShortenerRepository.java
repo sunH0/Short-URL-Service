777d9f8b206582d245ca78ac505055038ee2f5cf
healtheefart <961210eee@naver.com>
2022-06-02 06:59:45 +0900

    refactor : UrlShortenerService.java 전면적 refactoring - added @Transactional annotation - createNewUrl()을 통해 새로 url 단축로직 분리

--- src/main/java/com/mission/ShortURLService/repository/UrlShortenerRepository.java
+++ src/main/java/com/mission/ShortURLService/repository/UrlShortenerRepository.java
@@ -1,12 +1,14 @@
 package com.mission.ShortURLService.repository;
 
-import com.mission.ShortURLService.entity.ShortenUrl;
+import com.mission.ShortURLService.entity.Url;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UrlShortenerRepository extends JpaRepository<ShortenUrl, Integer> {
+public interface UrlShortenerRepository extends JpaRepository<Url, Integer> {
 
-	Optional<ShortenUrl> findByOriginUrl(String urlStr);
+	Optional<Url> findByOriginUrl(String urlStr);
+
+	Optional<Url> findByShortenUrl(String shortenUrl);
 }
--- src/main/java/com/mission/ShortURLService/service/UrlShortenerService.java
+++ src/main/java/com/mission/ShortURLService/service/UrlShortenerService.java
@@ -1,34 +1,44 @@
 package com.mission.ShortURLService.service;
 
-import com.mission.ShortURLService.dto.ShortenUrlRequest;
-import com.mission.ShortURLService.dto.ShortenUrlResponse;
-import com.mission.ShortURLService.entity.ShortenUrl;
+import com.mission.ShortURLService.dto.UrlShortenerRequest;
+import com.mission.ShortURLService.dto.UrlShortenerResponse;
+import com.mission.ShortURLService.entity.Url;
 import com.mission.ShortURLService.repository.UrlShortenerRepository;
 import com.mission.ShortURLService.utils.BASE62Utils;
 import java.util.Optional;
+import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
+@Transactional
 public class UrlShortenerService {
-
 	private final BASE62Utils base62Utils;
 	private final UrlShortenerRepository urlShortenerRepository;
 
-	public ShortenUrlResponse getShortenUrl(ShortenUrlRequest request) {
-		Optional<ShortenUrl> optional = urlShortenerRepository.findByOriginUrl(request.getOriginUrl());
-		if (optional.isPresent()) {
-			return new ShortenUrlResponse(
-				request.getOriginUrl(),
-				base62Utils.encode(optional.orElseThrow(() -> new RuntimeException()).getId()));
-		}
-		ShortenUrl shortenUrl = urlShortenerRepository.save(new ShortenUrl(request.getOriginUrl()));
-		return new ShortenUrlResponse(request.getOriginUrl(), base62Utils.encode(shortenUrl.getId()));
+	public UrlShortenerResponse getShortenUrl(UrlShortenerRequest request) {
+		Optional<Url> optional = urlShortenerRepository.findByOriginUrl(request.getOriginUrl());
+		return new UrlShortenerResponse(
+			base62Utils.encode(optional.orElseGet(() -> createNewUrl(request))
+									   .getId()
+									   .toString()));
 	}
 
-	public String getOriginUrl(String encodedUrl) {
-		Optional<ShortenUrl> optional = urlShortenerRepository.findById(base62Utils.decode(encodedUrl));
+	public String getOriginUrl(String shortenUrl) {
+		// TODO: 2022-06-02 url combine
+		Optional<Url> optional = urlShortenerRepository.findByShortenUrl(
+			"http://localhost:8080/redirect/" + shortenUrl);
+		// TODO: 2022-06-02 THAT URL'S NEVER BEEN SHORTEN
 		return optional.orElseThrow(() -> new RuntimeException()).getOriginUrl();
+	}
+
+	private Url createNewUrl(UrlShortenerRequest request) {
+		Url url = urlShortenerRepository.save(new Url(request.getOriginUrl()));
+		log.info("url Id : {}", url.getId());
+		url.setShortenUrl(base62Utils.encode(url.getId().toString()));
+		return url;
 	}
 }
